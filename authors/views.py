131dcbfdfc085b22cf6f3bd33f5f9e294cd76998
syzdeek <szymsx@gmail.com>
2022-08-10 11:12:18 +0200

    widok html authors

--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from authors.models import AuthorModel
+
 # Create your views here.
 def view_list(request):
+    authors = AuthorModel.objects.all()
     return render(
         request=request,
         template_name='authors/view_list.html',
+        context={
+            'authors' : authors
+        }
         
         )
