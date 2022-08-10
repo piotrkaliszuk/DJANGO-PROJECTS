bee9b449a7e8a4ff28c4c692f73b7007c40da623
piotrkaliszuk <kaliszuk_piotr@wp.pl>
2022-08-10 10:41:18 +0200

    models book

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import BookModel
+
 
 # Register your models here.
+
+
+@admin.register(BookModel)
+class BookAdmin(admin.ModelAdmin):
+    pass
--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class BookModel(models.Model):
+    title = models.CharField(max_length=200,blank=False)
+    rok_wydania = models.IntegerField(max_length=100,blank=True)
+    opis = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.title}, Rok {self.rok_wydania}, {self.opis}"
+
+
